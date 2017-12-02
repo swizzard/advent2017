09ac6d2365f810d682b0f992640c5945e0b857ab
sam raker <sam.raker@gmail.com>
2017-12-02 12:27:38 -0600

    day 2 part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,5 +1,7 @@
 module Day2 where
 
+import Data.Maybe (listToMaybe)
+
 getRows :: IO [[Int]]
 getRows = ((map . map) read . map words . lines) <$> readFile "data/day2.txt"
 
@@ -11,3 +13,12 @@ diff xs = let h = head xs in go xs h h where
                   | otherwise = go xs mx mn
 
 main = (sum . map diff) <$> getRows
+
+evenQ :: [Int] -> Int
+evenQ xs = let gq = [(fromIntegral a) / fromIntegral b
+                     | a <- xs, b <- xs, a `rem` b == 0, a /= b] in
+                   case listToMaybe gq of
+                     Nothing -> 0
+                     Just x -> truncate x
+
+main2 = (sum . map evenQ) <$> getRows
